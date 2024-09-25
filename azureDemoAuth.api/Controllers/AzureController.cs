03449d2fbc3385c5f33c495c75f3c4076ec02b65
fausio luis <fausioluis@live.com>
2024-09-25 22:48:33 +0200

    Add project files.

---
+++ azureDemoAuth.api/Controllers/AzureController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace azureDemoAuth.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    //[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:scopes")]
+    public class AzureController : ControllerBase
+    {
+
+
+        [HttpGet("[action]")]
+        public IActionResult GetData() {
+
+            var currentUser = User;
+
+            var data = new List<string>()
+            {
+                "a",
+                "b",
+                "c"
+            };
+
+            return  Ok(data);
+        
+        }
+
+
+        [Authorize(Roles = "DSI")]
+        [HttpGet("[action]")]
+        public IActionResult GetDataDSI()
+        {
+
+            var currentUser = User;
+
+            var data = new List<string>()
+            {
+                "a",
+                "b",
+                "c"
+            };
+
+            return Ok(data);
+
+        }
+    }
+}
